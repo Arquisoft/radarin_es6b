bde79aa49bc4287226fbd957bdb7c9c0dbc13bae
uo271246 <uo271246@uniovi.es>
2021-04-05 17:38:26 +0200

    Amigos en el menú

--- webapp/src/components/views/FriendsView.js
+++ webapp/src/components/views/FriendsView.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Typography } from '@material-ui/core/';
- 
+import useProfile from "../utils/Profile";
+import {useWebId} from '@solid/react';
+import ProfileImage from '../utils/ProfileImage';
 
-function FriendsView(){
-        return (
+function FriendsView(fullName, imageSrc){
+    const webId = useWebId();
+    const profile = useProfile(webId);   
+    return (
             <React.Fragment>
-             <Typography variant="h2">Friends</Typography>
+             <Typography variant="h2">
+             <ProfileImage fullName={
+                 useProfile(profile.friends).fullName
+                 } imageSrc={useProfile(profile.friends).imageSrc} />
+             </Typography>
             </React.Fragment>
         );
 }
