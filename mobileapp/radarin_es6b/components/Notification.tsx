8235b4ec7e482a525ad32407bff18aecc4993263
uo269948 <uo269948@uniovi.es>
2021-04-07 13:19:41 +0200

    mapa con notificaciones

--- mobileapp/radarin_es6b/components/Notification.tsx
+++ mobileapp/radarin_es6b/components/Notification.tsx
@@ -45,35 +45,29 @@ export default function Notification() {
   }, []);
   if (!isLoadingComplete) {
     return null;
-  } else {
-   /*  return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider > */
-    
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      }}>
-      <Text>Your expo push token: {expoPushToken}</Text>
-      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Title: {notification && notification.request.content.title} </Text>
-        <Text>Body: {notification && notification.request.content.body}</Text>
-        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-      </View>
-      <Button
-        title="Press to schedule a notification"
-        onPress={async () => {
-          await schedulePushNotification();
-        }}
-      />
-    </View>
-  );
-      }
+  } else {    
+        return (
+            <View
+            style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'space-around',
+            }}>
+            <Text>Your expo push token: {expoPushToken}</Text>
+            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Title: {notification && notification.request.content.title} </Text>
+                <Text>Body: {notification && notification.request.content.body}</Text>
+                <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+            </View>
+            <Button
+                title="Press to schedule a notification"
+                onPress={async () => {
+                await schedulePushNotification();
+                }}
+            />
+            </View>
+        );
+    }
 }
 async function schedulePushNotification() {
   await Notifications.scheduleNotificationAsync({
