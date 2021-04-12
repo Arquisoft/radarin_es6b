f83a30682fedd32893f0f31d979ac054b4c690ad
UO265336 <UO265336@uniovi.es>
2021-04-12 23:27:12 +0200

    Cambios en el about de la web

--- webapp/src/components/views/AboutView.js
+++ webapp/src/components/views/AboutView.js
@@ -1,12 +1,56 @@
 import React from 'react';
-import { Typography } from '@material-ui/core/';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-function AboutView(){
-        return (
-            <React.Fragment>
-                <Typography variant="h2">About</Typography>
-            </React.Fragment>
-        );
+const useStyles = makeStyles(() => ({
+    media: {
+        objectFit: 'contain'
+    },
+    parrafoEnLinea:{
+        display: 'inline'
+    }
+}));
+
+function AboutView() {
+    const classes = useStyles();
+
+    return <Grid container spacing={3} alignItems="center" justify="center" direction="column">
+        <Grid container item xs={12} sm={9} md={6} spacing={0} justify="center"
+            alignItems="stretch"
+            direction="column">
+            <Card>
+                <CardMedia
+                    component="img"
+                    height="70"
+                    className={classes.media}
+                    alt="Solid Logo"
+                    image="https://solid.mit.edu/assets/img/solid-logo.svg"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        About
+          </Typography>
+                    <Typography component="p">
+                        Radarin is a system to facilitate meetings between friends using new
+                        technologies. The application will be able to access your location if
+                        you voluntarily have it activated. It will allow other users who are
+                        your friends to know when you are nearby. You will be notified when
+                        you have friends nearby so that you can get in touch. The application
+                        will not store personal information but you will be asked for
+                        permission to obtain the information needed.
+                        </Typography>
+                    <Typography component="p" className={classes.parrafoEnLinea}>
+                    You can see the documentation of the application by clicking
+                    </Typography>
+                    <a href="https://radarines6bwebapp.herokuapp.com/docs/"> here.</a>
+                </CardContent>
+            </Card>
+        </Grid>
+    </Grid>;
 }
 
 export default AboutView;
