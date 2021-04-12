76caabfb2f71b90885bfbab03b1620327564721e
uo269948 <uo269948@uniovi.es>
2021-04-12 18:44:55 +0200

    probando tests

--- webapp/src/tests/NavComponent.test.js
+++ webapp/src/tests/NavComponent.test.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ReactTestUtils from "react-dom/test-utils";
+import NavBar from "../components/views/NavBar";
 import { render, fireEvent } from "@testing-library/react";
-import NavComponent from "../components/NavComponent";
 
-
-test('check that LogIn is rendering propertly', async () => {
-    const { getByText } = render(<NavComponent/>);
-    
+test('check that NavBar is rendering propertly', async () => {
+    const { getByText } = ReactDOM.render(<NavBar/>);
+    expect(getByText("")).toBeInTheDocument();
   });
