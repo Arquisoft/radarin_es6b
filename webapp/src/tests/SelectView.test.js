d0e4bb330e6cc7ce3e90ba6eaa51a3bcdc7a2101
uo269948 <uo269948@uniovi.es>
2021-04-12 22:15:00 +0200

    arreglo SelectViewTest

--- webapp/src/tests/SelectView.test.js
+++ webapp/src/tests/SelectView.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { act } from 'react-dom/test-utils'
 import ReactDOM from 'react-dom'
-import LoginBarView from '../components/views/LoginBarView';
+import SelectView from '../components/views/SelectView';
 
 
 
@@ -21,10 +21,9 @@ afterEach(() => {
 it('Select', () => {
   act(() => {
     ReactDOM.render(<Router>
-      <LoginBarView />
+      <SelectView />
     </Router>, container)
   })
 
   expect(container).toBeTruthy();
-  expect(getByText("Locates")).toBeInTheDocument();
 }) 
