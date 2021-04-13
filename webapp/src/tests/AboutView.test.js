64a8a6167eabd7c5c0c378a791a6a7351c54a305
uo269948 <uo269948@uniovi.es>
2021-04-13 17:16:26 +0200

    pasando los tests

---
+++ webapp/src/tests/AboutView.test.js
@@ -0,0 +1,57 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AboutView from "../components/views/AboutView";
+import { act } from 'react-dom/test-utils';
+import { render, fireEvent } from "@testing-library/react";
+import { BrowserRouter as Router } from 'react-router-dom';
+
+let container
+
+
+beforeEach(() => {
+
+  container = document.createElement('div')
+
+  document.body.appendChild(container)
+
+})
+
+
+
+afterEach(() => {
+
+  document.body.removeChild(container)
+
+  container = null
+
+})
+
+
+
+it('AboutView', () => {
+
+  act(() => {
+
+    ReactDOM.render(<Router>
+
+      <AboutView />
+
+    </Router>, container)
+
+  });
+  const enlace = container.querySelector('a');  
+ 
+  act(() => {
+    enlace.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+    
+  });
+  
+  //expect(enlace).toBeCalledTimes(1);
+
+
+  expect(container.querySelector('#doc')).toBeTruthy();
+  expect(container).toBeTruthy();
+ 
+
+})
+
