70f95fab52ed4110623ec9d122cd306e54127c55
uo269948 <uo269948@uniovi.es>
2021-04-22 23:55:01 +0200

    prueba introduccion tests

--- restapi/tests/api.test.js
+++ restapi/tests/api.test.js
@@ -30,23 +30,22 @@ describe('user ', () => {
      * Test that we can list users without any error.
      */
     it('can be listed',async () => {
-        /*
+        
         const response = await request(app).get("/api/users/list");
         expect(response.statusCode).toBe(200);
-        */
+        
     });
 
     /**
      * Tests that a user can be created through the productService without throwing any errors.
      */
     it('can be created correctly', async () => {
-        /*
         username = 'Pablo'
         email = 'pablo@uniovi.es'
         const response = await request(app).post('/api/users/add').send({name: username,email: email}).set('Accept', 'application/json')
         expect(response.statusCode).toBe(200);
         expect(response.body.name).toBe(username);
         expect(response.body.email).toBe(email);
-        */
+        
     });
 });
