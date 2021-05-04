5484523ddb4080c38d710bdfd7d2a2d05c49d324
uo269948 <uo269948@uniovi.es>
2021-05-04 19:00:12 +0200

    anteriores tests

--- webapp/e2e/global-teardown.js
+++ webapp/e2e/global-teardown.js
@@ -1,9 +1,5 @@
 const { teardown: teardownDevServer } = require("jest-dev-server")
-const fs = require('fs');
+
 module.exports = async () => {
     await teardownDevServer()
-    // cleanup Solid server data
-    if (fs.existsSync("./solid-server/data")) {
-        fs.rmdirSync("./solid-server/data", { recursive: true })
-    }
 }
