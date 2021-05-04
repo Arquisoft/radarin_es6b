ae5e00e9c96c4f9151b621e9b90e5117238ffc46
uo269948 <uo269948@uniovi.es>
2021-05-04 22:01:06 +0200

    Revert "Merge branch 'tests_uo269948'"
    This reverts commit 9d1ae1afeeba1a73c264d511187e9a02ab27b262, reversing
    changes made to 2baae8cccd1d7ede3750b16383ea6547181aeb18.

--- webapp/e2e/global-setup.js
+++ webapp/e2e/global-setup.js
@@ -1,27 +1,17 @@
 const { setup: setupDevServer } = require("jest-dev-server")
 module.exports = async () => {
     await setupDevServer([
-<<<<<<< HEAD
      /* {
-=======
-    {
->>>>>>> parent of 9d1ae1af (Merge branch 'tests_uo269948')
         command: 'node start-db.js',
         launchTimeout: 100000,
         debug:true,
         port: 27017,
-<<<<<<< HEAD
     }, 
      {
-=======
-    },
-    {
->>>>>>> parent of 9d1ae1af (Merge branch 'tests_uo269948')
         command: 'node start-restapi.js',
         launchTimeout: 60000,
         debug:true,
         port: 5000,
-<<<<<<< HEAD
     }  */
     /*  {
         command: process.platform === 'win32' ? 'SET BROWSER=none && npm start' : 'BROWSER=none npm start',
@@ -29,18 +19,12 @@ module.exports = async () => {
         debug: true,
         port: 3000
     } */
-=======
-    },
->>>>>>> parent of 9d1ae1af (Merge branch 'tests_uo269948')
     {
-        command: 'BROWSER=none npm start',
+        command: 'npm start',
         launchTimeout: 60000,
         debug: true,
         port: 3000
-<<<<<<< HEAD
     }
 ])  
-=======
-    }])
->>>>>>> parent of 9d1ae1af (Merge branch 'tests_uo269948')
 }
+© 2021 GitHub, Inc.
