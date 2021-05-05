35c5fe4127751a0539b5901df2011112860cb2fe
UO265336 <UO265336@uniovi.es>
2021-05-05 10:11:23 +0200

    incremento en el timeout

--- webapp/e2e/jest.config.js
+++ webapp/e2e/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     testEnvironment: './custom-environment.js',
     testMatch: ["**/steps/*.js"],
-    testTimeout: 800000,
+    testTimeout: 1000000,
     globalSetup: './global-setup.js',
     globalTeardown: './global-teardown.js',
     setupFilesAfterEnv: ["expect-puppeteer"]
