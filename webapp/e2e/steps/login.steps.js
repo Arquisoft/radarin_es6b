b8574d954968a3a2906b5bde6c442a6a4d3a1b17
uo269948 <uo269948@uniovi.es>
2021-05-05 18:04:53 +0200

    e2e

--- webapp/e2e/steps/login.steps.js
+++ webapp/e2e/steps/login.steps.js
@@ -36,7 +36,7 @@ test("We want to login into radarin", ({given, when, then})=> {
     });
 
     then("I expect to be on HomeView of radarin", async ()=> {
-        await expect(page).toMatch("Mapa", {waitUntil: "load", timeout:0});
+        await expect(page).toMatch("Geolocation is not supported by this browser!", {waitUntil: "load", timeout:0});
     });
 });
 });
