6ff404514a3ae5edb421567ad945564b89fe9159
UO265336 <UO265336@uniovi.es>
2021-05-05 12:30:51 +0200

    Guargar por primera vez la localizacion

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import Contaniner from './components/utils/Contenedor';
 import { useWebId } from '@solid/react';
 import { saveUser } from './api/api';
@@ -9,8 +9,9 @@ import { saveUser } from './api/api';
 function App() {
 
   const webId = useWebId();
+  const [firstSave, setFirstSave] = useState(false);
 
-  const saveLocateUser= useCallback(function() {
+  const saveLocateUser = useCallback(function () {
     if (webId) {
       // pedimos la pocalización actual
       navigator.geolocation.getCurrentPosition((pos) => {
@@ -19,13 +20,29 @@ function App() {
         saveUser(webId, pos.coords.latitude, pos.coords.longitude).catch(err => console.log(err));
       });
     }
-  },[webId]);
+  }, [webId]);
+
+
+  const saveFirstLocateUser = useCallback(function () {
+    if (webId) {
+      // pedimos la pocalización actual
+      navigator.geolocation.getCurrentPosition((pos) => {
+
+        //salvamos al usuario
+        saveUser(webId, pos.coords.latitude, pos.coords.longitude).catch(err => console.log(err));
+      });
+      setFirstSave(true);
+    }
+  }, [webId]);
 
 
   useEffect(() => {
+    if (!firstSave) {
+      saveFirstLocateUser();
+    }
     // Guardamos la localización cada 20 segundos
     setInterval(saveLocateUser, 20000);
-  });
+  }, [saveFirstLocateUser, saveLocateUser,firstSave]);
 
 
   return (
