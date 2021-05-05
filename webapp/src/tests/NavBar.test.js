32fcb300f70ebb37ea6a140763941ff4d417d157
uo269948 <uo269948@uniovi.es>
2021-05-06 00:09:41 +0200

    subiendo codecov

--- webapp/src/tests/NavBar.test.js
+++ webapp/src/tests/NavBar.test.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { act } from 'react-dom/test-utils'
 import ReactDOM from 'react-dom'
-import LoginBarView from '../components/views/LoginBarView';
-import ProfileImage from '../components/utils/solid/ProfileImage';
+import NavBar from '../components/views/NavBar';
+import TemporaryDrawer from '../components/utils/navbar/TemporaryDrawer';
+import GetProfile from '../components/utils/solid/GetProfile'
 import { render } from "@testing-library/react";
 
 
@@ -19,13 +20,17 @@ afterEach(() => {
   container = null
 })
 
-it('LoginBar', () => {
+it('NavBar', () => {
   act(() => {
     ReactDOM.render(<Router>
-      <LoginBarView />
+      <NavBar />
     </Router>, container)
-    render(<ProfileImage /> );
+    render(<TemporaryDrawer /> );
   })
 
   expect(container).toBeTruthy();
-}) 
+}); 
+test("NavBar view", async () => {
+    
+    render(<GetProfile /> );
+})
