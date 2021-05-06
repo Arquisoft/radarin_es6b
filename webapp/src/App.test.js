b4802d1b1bdab3644d26bfcfc301810687801c88
uo269948 <uo269948@uniovi.es>
2021-05-06 11:51:58 +0200

    despliegue

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import ReactDOM from "react-dom";
 import { act } from 'react-dom/test-utils';
+import App from "./App";
 
 let container
 
@@ -25,5 +26,8 @@ it('App', () => {
 })
 
 
+test("App", async () => {
+  ReactDOM.render(
+    <App />, container)
+})
   
-
